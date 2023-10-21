21afa12d2a57bbc230084a3652cb8a4112cdc3e4
Jonathan Hiles <jonathan@hil.es>
2023-10-22 00:01:37 +1000

    feat: validate category and pack display names

--- README.md
+++ README.md
@@ -13,23 +13,62 @@ packs.
 poetry install
 ```
 
+### 🚀 Usage
+
+```sh
+$ vanilla-tweaks-partition --help
+
+ Usage: vanilla-tweaks-partition [OPTIONS] 📦 PACKS...
+
+ Download Vanilla Tweaks packs into separate Minecraft resource packs.
+
+╭─ Arguments ────────────────────────────────────────────────────────────────────────────────────────────────────────╮
+│ *    packs      📦 PACKS...  Vanilla Tweaks pack names in format 'Category/Pack Name'. [required]                  │
+╰────────────────────────────────────────────────────────────────────────────────────────────────────────────────────╯
+╭─ Options ──────────────────────────────────────────────────────────────────────────────────────────────────────────╮
+│ --mc-version                TEXT  The Minecraft version. [default: 1.20]                                           │
+│ --outdir                    PATH  The pack output directory. [default: packs]                                      │
+│ --install-completion              Install completion for the current shell.                                        │
+│ --show-completion                 Show completion for the current shell, to copy it or customize the installation. │
+│ --help                            Show this message and exit.                                                      │
+╰────────────────────────────────────────────────────────────────────────────────────────────────────────────────────╯
+```
+
 ### 🪄 Example
 
 ```sh
-vanilla-tweaks-partition \
-  aesthetic/BetterParticles \
-  utility/FullAgeAmethystMarker \
-  utility/FullAgeCropMarker \
-  utility/NoteblockBanners \
-  utility/VisualHoney \
-  utility/BetterObservers \
-  utility/DirectionalDispensersDroppers \
-  utility/StickyPistonSides \
-  utility/SuspiciousSandGravelBorders \
-  utility/OreBorders \
-  unobtrusive/LowerShield \
-  unobstrusive/InvisibleTotem \
-  hud/PingColorIndicator
+$ vanilla-tweaks-partition \
+    'Aesthetic/Colorful Enchanting Table Particles' \
+    'HUD/Ping Color Indicator' \
+    'Unobtrusive/Lower Shield' \
+    'Unobtrusive/Invisible Totem' \
+    'Utility/Directional Dispensers & Droppers' \
+    'Utility/Directional Observers' \
+    'Utility/Fully Aged Amethyst Cluster Marker' \
+    'Utility/Fully Aged Crop Marker' \
+    'Utility/Ore Borders' \
+    'Utility/Sticky Piston Sides' \
+    'Utility/Suspicious Sand & Gravel Borders' \
+    'Utility/Visual Honey Stages' \
+    'Utility/Visual Note Block Pitch'
+
+📦 Requested 13 Vanilla Tweaks pack/s
+
+  Aesthetic » Colorful Enchanting Table Particles ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 100%
+  Utility » Fully Aged Amethyst Cluster Marker    ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 100%
+  Utility » Fully Aged Crop Marker                ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 100%
+  Utility » Visual Note Block Pitch               ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 100%
+  Utility » Visual Honey Stages                   ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 100%
+  Utility » Directional Observers                 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 100%
+  Utility » Directional Dispensers & Droppers     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 100%
+  Utility » Sticky Piston Sides                   ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 100%
+  Utility » Suspicious Sand & Gravel Borders      ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 100%
+  Utility » Ore Borders                           ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 100%
+  Unobtrusive » Lower Shield                      ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 100%
+  Unobtrusive » Invisible Totem                   ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 100%
+  HUD » Ping Color Indicator                      ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 100%
+
+🎉 Success!
 ```
 
 [vanillatweaks]: https://vanillatweaks.net/picker/resource-packs/
--- src/vanilla_tweaks_partition/main.py
+++ src/vanilla_tweaks_partition/main.py
@@ -1,45 +1,98 @@
-from __future__ import annotations
-
 import json
-import re
 import zipfile
 from concurrent.futures import ThreadPoolExecutor
 from io import BytesIO
+from itertools import groupby
+from operator import itemgetter
 from pathlib import Path
-from typing import Final, Any, Optional
+from typing import Annotated, Any, Final, Optional
 
 import requests
 import typer
-from rich.progress import SpinnerColumn, Progress, TextColumn, TaskProgressColumn, TaskID
-from typer import Typer
+from rich import print
+from rich.progress import BarColumn, Progress, SpinnerColumn, TaskProgressColumn, TextColumn
+from slugify import slugify
+from typer import Argument, Option, Typer
 
 # The Typer application
 app: Final[Typer] = typer.Typer()
 
+# The default Minecraft version
+DEFAULT_MC_VERSION: Final[str] = "1.20"
+# The default output directory for generated packs
+DEFAULT_PACK_DIR: Final[Path] = Path("./packs/")
+
 
 @app.command()
-def generate(packs: list[str], mc_version: str = "1.20", outdir: Optional[Path] = None) -> None:
+def generate(
+    packs: Annotated[
+        list[str],
+        Argument(
+            metavar="📦 PACKS...",
+            help="Vanilla Tweaks pack names in format 'Category/Pack Name'.",
+            show_default=False,
+        ),
+    ],
+    mc_version: Annotated[str, Option(help="The Minecraft version.")] = DEFAULT_MC_VERSION,
+    outdir: Annotated[Optional[Path], Option(help="The pack output directory.")] = DEFAULT_PACK_DIR,
+) -> None:
     """Download Vanilla Tweaks packs into separate Minecraft resource packs."""
 
-    if not outdir:
-        outdir = Path("./packs")
+    print(f"\n:package: Requested [blue bold]{len(packs)}[/blue bold] Vanilla Tweaks pack/s\n")
+
     outdir.mkdir(exist_ok=True)
 
+    # Fetch pack definitions
     with Progress(
         SpinnerColumn(),
         TextColumn("[progress.description]{task.description}"),
         TaskProgressColumn(),
+        transient=True,
     ) as progress:
-        def process(category: str, pack: str):
+        # Fetch pack definitions
+        task = progress.add_task("Fetching pack definitions", total=0)
+        res = requests.get(f"https://vanillatweaks.net/assets/resources/json/{mc_version}/rpcategories.json")
+        res.raise_for_status()
+        metadata: dict[str, Any] = res.json()
+        categories: dict[str, dict[str, Any]] = {  # {category: {pack: {...}, ...}, ...}
+            entry["category"]: {item["display"]: item for item in entry["packs"]} for entry in metadata["categories"]
+        }
+        progress.remove_task(task)
+
+        # Validate requested packs
+        task = progress.add_task("Validating requested packs...", total=len(packs))
+        chosen_packs: dict[str, dict[str, Any]] = {}  # {category: {pack: {...}, ...}, ...}
+        for category, items in groupby([pack.split("/", 1) for pack in packs], itemgetter(0)):
+            if category in categories:
+                chosen_packs[category] = {}
+                for _, item in items:
+                    if item in categories[category]:
+                        chosen_packs[category][item] = categories[category][item]
+                        progress.advance(task, 1)
+                    else:
+                        print(f"[red]Unknown pack:[/red] [dim]{category} »[/dim] {item}")
+                        exit(1)
+            else:
+                print(f"[red]Unknown category:[/red] {category}")
+                exit(1)
+
+    # Download and adjust each pack
+    with Progress(
+        SpinnerColumn(),
+        TextColumn("[progress.description]{task.description}"),
+        BarColumn(),
+        TaskProgressColumn(),
+    ) as progress:
+
+        def process(category: str, pack: dict[str, Any]) -> None:
             # Track the task progress
-            category_display: str = re.sub(r"(\w)([A-Z])", r"\1 \2", category).title()
-            pack_display: str = re.sub(r"(\w)([A-Z])", r"\1 \2", pack).title()
-            task = progress.add_task(f"[gray]{category_display} »[/gray] {pack_display}", total=4)
+            category_slug: str = slugify(category)
+            task = progress.add_task(f"[dim]{category} »[/dim] {pack['display']}", total=4)
 
             # Generate the pack archive
             res = requests.post(
                 "https://vanillatweaks.net/assets/server/zipresourcepacks.php",
-                data={"version": mc_version, "packs": json.dumps({category: [pack]})},
+                data={"version": mc_version, "packs": json.dumps({category_slug: [pack["name"]]})},
             )
             res.raise_for_status()
             progress.advance(task, 1)
@@ -51,33 +104,38 @@ def generate(packs: list[str], mc_version: str = "1.20", outdir: Optional[Path] 
             progress.advance(task, 1)
 
             # Fetch the actual pack image
-            res = requests.get(f"https://vanillatweaks.net/assets/resources/icons/resourcepacks/{mc_version}/{pack}.png?v1")
+            res = requests.get(
+                f"https://vanillatweaks.net/assets/resources/icons/resourcepacks/{mc_version}/{pack['name']}.png?v1"
+            )
             res.raise_for_status()
             img: bytes = res.content
             progress.advance(task, 1)
 
             # Modify the archived pack
-            with zipfile.ZipFile(pack_content, "r") as original:
-                with zipfile.ZipFile(outdir / f"Vanilla Tweaks - {pack_display}.zip", mode="w") as archive:
-                    # Load the pack metadata
-                    with original.open("pack.mcmeta", "r") as file:
-                        content: dict[str, Any] = json.loads(file.read())
-                    # Update the pack description
-                    with archive.open("pack.mcmeta", "w") as file:
-                        content["pack"]["description"] = f"{category_display} / {pack_display}\nvanillatweaks.net"
-                        file.write(json.dumps(content, indent=2).encode())
-                    # Update the pack image
-                    archive.writestr("pack.png", img)
-                    # Copy remaining files
-                    for file in original.filelist:
-                        if file.filename not in ("pack.mcmeta", "pack.png"):
-                            archive.writestr(file, original.read(file))
+            with zipfile.ZipFile(pack_content, "r") as original, zipfile.ZipFile(
+                outdir / f"Vanilla Tweaks - {pack['display']}.zip", mode="w"
+            ) as archive:
+                # Load the pack metadata
+                with original.open("pack.mcmeta", "r") as file:
+                    content: dict[str, Any] = json.loads(file.read())
+                # Update the pack description
+                with archive.open("pack.mcmeta", "w") as file:
+                    content["pack"]["description"] = f"{pack['display']}\nvanillatweaks.net"
+                    file.write(json.dumps(content, indent=2).encode())
+                # Update the pack image
+                archive.writestr("pack.png", img)
+                # Copy remaining files
+                for file in original.filelist:
+                    if file.filename not in ("pack.mcmeta", "pack.png"):
+                        archive.writestr(file, original.read(file))
             progress.advance(task, 1)
 
-        with ThreadPoolExecutor(max_workers=3) as pool:
-            for item in packs:
-                category_name, pack_name = item.split("/", 1)
-                pool.submit(process, category=category_name, pack=pack_name)
+        with ThreadPoolExecutor(max_workers=5) as pool:
+            for category_name, entries in chosen_packs.items():
+                for pack_data in entries.values():
+                    pool.submit(process, category=category_name, pack=pack_data)
+
+    print(f"\n:tada: [green]Success![/green] [link={outdir.absolute()}]{outdir.absolute()}[/link]")
 
 
 if __name__ == "__main__":
